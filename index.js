559f081d8667e7310ca8ab3facac347adf1a1b24
davorinpillz <ddp1329@gmail.com>
2024-11-16 11:29:20 +0100

    complete

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const app = express();
 
-var app = express();
+require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
+require('dotenv').config()
+mongoose.connect('mongodb+srv://ddp1329:clifton84@cluster0.x0i7l.mongodb.net/uploads?retryWrites=true&w=majority&appName=Cluster0')
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +20,25 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+console.log("tetsetest")
 //test
+
+const Schema = mongoose.Schema
+const fileSchema = new Schema({
+  name: String,
+  type: String,
+  size: Number
+})
+const File = mongoose.model("File", fileSchema)
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  let newFile = new File({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+  newFile.save()
+  res.json(newFile)
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  console.log(req.file)
+})
 
 
 const port = process.env.PORT || 3000;
